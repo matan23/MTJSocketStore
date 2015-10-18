a71d0da605a0cd64a8d100cef7064021069782f7
mataejoon <boitedechocolat@Mathieus-MacBook-Air.local>
2015-10-18 23:33:53 +0200

    example view controller

--- MTJSocketStore/Example/ViewController.m
+++ MTJSocketStore/Example/ViewController.m
@@ -8,15 +8,45 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "MTJSocketStore.h"
+#import "MTJSyncedTableViewDataSource.h"
+
+#import "Conversation.h"
+
+@interface ViewController () <MTJSyncedTableViewDataSourceDelegate> {
+    MTJSyncedTableViewDataSource *_dataSource;
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 44, 0);
+    
+    _dataSource = [[MTJSocketStore sharedStore] tableViewDataSourceForType:(id<MTJSyncedEntity>)[Conversation class]];    
+    [_dataSource setDelegate:self tableView:_tableView withCellIdentifier:@"Cell"];
+    
+    _tableView.dataSource = _dataSource;
+    
+    [_dataSource sync];
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    if (self.tableView.contentSize.height > self.tableView.frame.size.height)
+    {
+        CGPoint offset = CGPointMake(0, self.tableView.contentSize.height - self.tableView.frame.size.height + 44);
+        [self.tableView setContentOffset:offset animated:YES];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +54,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)configureCell:(UITableViewCell *)cell withObject:(id)object {
+    Conversation *conv = object;
+    cell.textLabel.text = conv.obj_id;
+}
+
+- (void)deleteObject:(id)object {
+//    nothing for now
+}
 @end
